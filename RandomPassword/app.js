e279b118058e69b39372f9eda89f19d4de96c3de
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-11-15 18:44:41 +0500

    Add files via upload

--- RandomPassword/app.js
+++ RandomPassword/app.js
@@ -2,20 +2,53 @@ var small = "abcdefghijklmnopqrstuvwxyz";
 var capital = small.toUpperCase();
 var num = "123456789";
 var symbols = "!@#$%^&*?><";
-var all = small + capital + num + symbols; // length 72
+var all = small + capital + num + symbols; 
 var password = "";
-var strong = true;
+var strongLevel = 0;
+var strong = false;
 
-for (i = 0; i < 12; i++) {
-  password += all[Math.floor(Math.random() * 72)];
+for (var i = 0; i < 12; i++) {
+  password += all[Math.floor(Math.random() * all.length)];
 }
-for (i = 0; i < password.length; i++) {
-//   if (password[i] !== num.indexOf(-1)) {
-//     strong = false;
-//     alert("strong");
-//   } else {
-//     alert("week");
-//   }
 
-//   console.log(strong);
+
+
+
+
+for (var j = 0; j < password.length; j++) {
+  // console.log(password[j],num.indexOf(password[j]));
+  if (capital.indexOf(password[j]) !== -1) {
+    strongLevel++
+    break
+  }
+
 }
+for (var k = 0; k < password.length; k++) {
+
+  if (small.indexOf(password[k]) !== -1) {
+    strongLevel++
+    break
+  }
+}
+for (var l = 0; l < password.length; l++) {
+
+  if (num.indexOf(password[l]) !== -1) {
+
+    strongLevel++
+    break
+  }
+}
+
+for (var m = 0; m < password.length; m++) {
+
+  if (symbols.indexOf(password[m]) !== -1) {
+
+    strongLevel++
+    break
+  }
+}
+
+if(strongLevel === 4){
+  strong = true
+}
+console.log("strong==> " + strong, password)

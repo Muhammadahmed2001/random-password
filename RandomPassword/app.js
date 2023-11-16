1656bd6dafae00d4a0a7464a89260d7ccb08c4f5
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-11-16 19:24:21 +0500

    Add files via upload

--- RandomPassword/app.js
+++ RandomPassword/app.js
@@ -1,54 +1,100 @@
+
+
+
+
+
+
+
 var small = "abcdefghijklmnopqrstuvwxyz";
 var capital = small.toUpperCase();
 var num = "123456789";
 var symbols = "!@#$%^&*?><";
-var all = small + capital + num + symbols; 
+var all = small + capital + num + symbols;
 var password = "";
 var strongLevel = 0;
 var strong = false;
+var passwordInput = document.getElementById("password-input")
+var copied = document.querySelector('.copied')
 
-for (var i = 0; i < 12; i++) {
-  password += all[Math.floor(Math.random() * all.length)];
+// console.log(copied.innerHTML)
+
+function generatePassword() {
+  copied.style.display = "none"
+  password = ""
+  for (var i = 0; i < 12; i++) {
+    password += all[Math.floor(Math.random() * all.length)];
+  }
+  
+  passwordInput.innerText = password
 }
 
 
 
-
-
-for (var j = 0; j < password.length; j++) {
-  // console.log(password[j],num.indexOf(password[j]));
-  if (capital.indexOf(password[j]) !== -1) {
-    strongLevel++
-    break
+var passwordStrength = document.getElementById("password-strength")
+function copyText(){
+  if(password !== ""){
+    copied.style.display = "block"
+    navigator.clipboard.writeText(passwordInput.innerText)
+    passwordStrength.innerHTML = password
+  }else{
+    alert("enter value")
   }
 
 }
-for (var k = 0; k < password.length; k++) {
 
-  if (small.indexOf(password[k]) !== -1) {
-    strongLevel++
-    break
-  }
-}
-for (var l = 0; l < password.length; l++) {
 
-  if (num.indexOf(password[l]) !== -1) {
 
-    strongLevel++
-    break
-  }
-}
 
-for (var m = 0; m < password.length; m++) {
 
-  if (symbols.indexOf(password[m]) !== -1) {
+// for (var j = 0; j < password.length; j++) {
+//   // console.log(password[j],num.indexOf(password[j]));
+//   if (capital.indexOf(password[j]) !== -1) {
+//     strongLevel++
+//     break
+//   }
 
-    strongLevel++
-    break
-  }
-}
+// }
+// for (var k = 0; k < password.length; k++) {
 
-if(strongLevel === 4){
-  strong = true
-}
-console.log("strong==> " + strong, password)
+//   if (small.indexOf(password[k]) !== -1) {
+//     strongLevel++
+//     break
+//   }
+// }
+// for (var l = 0; l < password.length; l++) {
+
+//   if (num.indexOf(password[l]) !== -1) {
+
+//     strongLevel++
+//     break
+//   }
+// }
+
+// for (var m = 0; m < password.length; m++) {
+
+//   if (symbols.indexOf(password[m]) !== -1) {
+
+//     strongLevel++
+//     break
+//   }
+// }
+
+// if(strongLevel === 4){
+//   strong = true
+// }
+// console.log("strong==> " + strong, password)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
